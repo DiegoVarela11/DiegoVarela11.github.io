1ce3d2409490ec0799221bc7ac6b8c57b947a771
DIEGO VARELA <diego.varela@uc.cl>
2024-11-17 22:26:38 -0300

    HW1 JS

---
+++ JS/HW1/JS/gallery.js
@@ -0,0 +1,13 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    big_pic = document.getElementById("image");
+    big_pic.innerHTML = previewPic.alt;
+    big_pic.style.backgroundImage = `url(${previewPic.src})`;
+}
+
+function unDo(){
+    big_pic = document.getElementById("image");
+    big_pic.innerHTML = "Hover over an image below to display here.";
+    big_pic.style.backgroundImage = "url('')";
+}
